65711808eab12befc6950ca507cc65c581809f01
alfredo-lefranc <alefranc@itam.mx>
2021-03-02 14:59:45 -0600

    carga archivo de prueba

---
+++ prueba_conficto/prueba.R
@@ -0,0 +1,24 @@
+# Script de prueba para probar solución de conflictos en Github
+
+
+# Carga las librerias
+library(dplyr)
+library(broom)
+library(stargazer)
+library(kableExtra)
+
+
+# Carga la base de datos
+base<- mtcars
+
+# Guarda las columnas
+columnas <- (as.data.frame(colnames(base)))
+
+# Crea una tabla
+kable(columnas, booktabs=T, align = 'c', col.names = c("Columnas"), longtable=T) %>%
+  kable_styling(position = "center",latex_options = "repeat_header")
+
+# Algunos datos de interes
+sum(as.numeric(duplicated(base)))
+summary(base)
+
